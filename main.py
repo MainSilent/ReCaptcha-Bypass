c607f9a6ba4854fa06cf745ca171d75941679718
7BxDWoKVT4 <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-21 19:01:09 +0430

    Update main.py

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ if audioBtnFound:
             inputbtn.send_keys(Keys.ENTER)
 
             time.sleep(2)
-            errorMsg = driver.find_element_by_class_name('recaptcha-checkbox-border')
+            checkbox = driver.find_element_by_class_name('recaptcha-checkbox-border')
 
-            if errorMsg.value_of_css_property('display') == 'none':
+            if checkbox.value_of_css_property('display') == 'none':
                 print("\033[32m"+"Success"+"\033[0m")
             else:
                  print("\033[31m"+"Failed"+"\033[0m")
