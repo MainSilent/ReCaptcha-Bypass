87ac8dd4a60212c4c01b2e628ce058764275d28c
7BxDWoKVT4 <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-21 20:12:23 +0430

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ googleIBMLink = 'https://speech-to-text-demo.ng.bluemix.net/'
 option = webdriver.ChromeOptions()
 option.add_argument('--disable-notifications')
 option.add_argument("--mute-audio")
-option.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1")
 
 def saveFile(content,filename):
     with open(filename, "wb") as handle:
@@ -85,4 +84,4 @@ if audioBtnFound:
 else:
     print('Button not found. This should not happen.')
 
-driver.close()
+#driver.close()
